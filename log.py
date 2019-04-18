2e71cf19cf95847d5f8600232de2dec396da894d
shohoku3 <syf19961002@outlook.com>
2019-04-18 21:50:55 +0800

    log.py

--- log.py
+++ log.py
@@ -3,6 +3,7 @@ import urllib.parse
 import requests
 from selenium import webdriver
 from selenium.webdriver.support.ui import Select
+import time
 
 #TODO：reader csv file
 for csvNum in range(10):
@@ -16,17 +17,29 @@ for csvNum in range(10):
 	resultCol4File.write('\n')
 	#TODO 模拟浏览器并提交数据
 	url='https://www.cmd5.com/'
-	brower=webdriver.Firefox()
+	brower=webdriver.Chrome()
 	brower.get(url)
-	for row in LogReader:
-		#TODO: IP
+	#TODO:模拟登录
+	loginBtnElem=brower.find_element_by_link_text('qq一键登录')
+	loginBtnElem.click()
+		#切换iframe
+	brower.switch_to_frame('ptlogin_iframe')
+	qqLoginBtnElem=brower.find_element_by_link_text('帐号密码登录')
+	qqLoginBtnElem.click()
+	qqInputElem=brower.find_element_by_id('u')
+	qqInputElem.send_keys('2812845017')
+	qqPsdInputElem=brower.find_element_by_id('p')
+	qqPsdInputElem.send_keys('syf199610021016')
+	qqBtnElem=brower.find_element_by_id('login_button')
+	qqBtnElem.click()
+	time.sleep(5)
+	for row in LogReader:	#TODO: IP
 
 		#TODO: 在线查询MD5
-		#TODO:将待处理的MD5数据切片
+			#TODO:将待处理的MD5数据切片
 		decodecol2=row[2].split('.')
 		resultCol2=str(decodecol2[1])
-		print(resultCol2)
-		#TODO:模拟提交表单
+			#TODO:模拟提交表单
 		inputElem=brower.find_element_by_id('ctl00_ContentPlaceHolder1_TextBoxInput')
 		inputElem.send_keys(resultCol2)
 		selectElm = Select(brower.find_element_by_id('ctl00_ContentPlaceHolder1_InputHashType'))
@@ -34,8 +47,14 @@ for csvNum in range(10):
 		btnElem=brower.find_element_by_id('ctl00_ContentPlaceHolder1_Button1')
 		btnElem.click()
 		answerElem=brower.find_element_by_id('LabelAnswer').text
-		print(answerElem)
+			#抓取的内容进行在线查询 获取信息
+			#TODO 内容写入文本
+		resultCol2File.write('www.'+answerElem+'.'+str(decodecol2[2])+'.com')
+		resultCol2File.write('\n')
+
 		#TODO: decode base64 and write in resultCol4
 		decodeCol4=base64.standard_b64decode(row[4])
 		resultCol4=urllib.parse.unquote(str(decodeCol4))
 		resultCol4File.write(resultCol4)
+
+		#TODO:
